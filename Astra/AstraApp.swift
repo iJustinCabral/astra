dbf75a43cbda5df2675217581bf9108ea44053f7
Justin Cabral <ijustincabral@gmail.com>
2022-12-31 22:43:54 -0500

    Initial Commit

---
+++ Astra/AstraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstraApp.swift
+//  Astra
+//
+//  Created by Justin Cabral on 12/31/22.
+//
+
+import SwiftUI
+
+@main
+struct AstraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
